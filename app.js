24ba640492e8a58eca47b3b7d6bdce58a3d42ce3
Lucas Vazquez <vazquez.lucas.alejo@gmail.com>
2024-11-28 17:20:41 -0300

    Switch idioma completo

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 document.getElementById("languageSwitch").addEventListener("change", function () {
-  const title = document.getElementById("title");
-  const description = document.getElementById("description");
   const presentacion = document.getElementById("presentacion");
   const Proyectos = document.getElementById("Proyectos");
   const Certificados = document.getElementById("Certificados");
@@ -16,6 +14,7 @@ document.getElementById("languageSwitch").addEventListener("change", function ()
   const BtnContacto = document.getElementById("BtnContacto");
 
   if (this.checked) {
+    localStorage.setItem("language", "en");
     BtnInicio.innerText ="Home";
     BtnSobreMi.innerText ="About me";
     BtnContacto.innerText ="Contact";
@@ -24,7 +23,7 @@ document.getElementById("languageSwitch").addEventListener("change", function ()
     presentacion.innerHTML = `
       <span>
         The page is currently under construction.<br><br>
-        I am a data analyst. I graduated as a University Technician in Programming at <a href="https://www.frt.utn.edu.ar/">UTN FRT</a>.<br>
+        I am a data analyst. I graduated as Associate's degree in university programming at <a href="https://www.frt.utn.edu.ar/">UTN FRT</a>.<br>
         Some of the skills I have include: Python, PowerBI, Looker Studio, Excel, Spreadsheets, MySQL, and inferential statistics.
       </span>`;
       DescProyecto1.innerText =`This is a more detailed description of the project. It can include information about the goals, 
@@ -43,6 +42,7 @@ document.getElementById("languageSwitch").addEventListener("change", function ()
       the technologies used, challenges faced, and the outcomes achieved.`;
     
   } else {
+    localStorage.setItem("language", "es");
     BtnInicio.innerText ="Inicio";
     BtnSobreMi.innerText ="Sobre mí";
     BtnContacto.innerText ="Contacto";
@@ -74,3 +74,62 @@ document.getElementById("languageSwitch").addEventListener("change", function ()
 
   
 
+window.addEventListener("DOMContentLoaded", () => {
+  const language = localStorage.getItem("language");
+  if (language === "en") {
+    document.getElementById("languageSwitch").checked = true;
+    document.getElementById("BtnInicio").innerText = "Home";
+    document.getElementById("BtnSobreMi").innerText = "About me";
+    document.getElementById("BtnContacto").innerText = "Contact";
+    Proyectos.innerText ="Projects";
+    Certificados.innerText = "Certifications";
+    presentacion.innerHTML = `
+      <span>
+        The page is currently under construction.<br><br>
+        I am a data analyst. I graduated as Associate's degree in university programming at <a href="https://www.frt.utn.edu.ar/">UTN FRT</a>.<br>
+        Some of the skills I have include: Python, PowerBI, Looker Studio, Excel, Spreadsheets, MySQL, and inferential statistics.
+      </span>`;
+      DescProyecto1.innerText =`This is a more detailed description of the project. It can include information about the goals, 
+      the technologies used, challenges faced, and the outcomes achieved.`;
+
+      DescProyecto2.innerText =`This is a more detailed description of the project. It can include information about the goals, 
+      the technologies used, challenges faced, and the outcomes achieved.`;
+
+      DescProyecto3.innerText =`This is a more detailed description of the project. It can include information about the goals, 
+      the technologies used, challenges faced, and the outcomes achieved.`;
+
+      DescProyecto4.innerText =`This is a more detailed description of the project. It can include information about the goals, 
+      the technologies used, challenges faced, and the outcomes achieved.`;
+
+      DescProyecto5.innerText =`This is a more detailed description of the project. It can include information about the goals, 
+      the technologies used, challenges faced, and the outcomes achieved.`;
+  } else {
+    document.getElementById("languageSwitch").checked = false;
+    document.getElementById("BtnInicio").innerText = "Inicio";
+    document.getElementById("BtnSobreMi").innerText = "Sobre mí";
+    document.getElementById("BtnContacto").innerText = "Contacto";
+    Proyectos.innerText ="Proyectos";
+    Certificados.innerText = "Certificados";
+    presentacion.innerHTML = `
+      <span>
+        La página actualmente se encuentra en construcción.<br><br>
+        Soy analista de datos. Recibido de Técnico Universitario en Programación en <a href="https://www.frt.utn.edu.ar/">UTN FRT</a>.<br>
+        Algunas de las skills que manejo son: Python, PowerBI, Looker Studio, Excel, Spreedsheets, MySql y estadística inferencial.
+      </span>`;
+      DescProyecto1.innerText =`Se trata de una descripción más detallada del proyecto.
+      Puede incluir información sobre los objetivos, las tecnologías utilizadas, los desafíos afrontados y los resultados obtenidos.`;
+
+      DescProyecto2.innerText =`Se trata de una descripción más detallada del proyecto.
+      Puede incluir información sobre los objetivos, las tecnologías utilizadas, los desafíos afrontados y los resultados obtenidos.`;
+
+      DescProyecto3.innerText =`Se trata de una descripción más detallada del proyecto.
+      Puede incluir información sobre los objetivos, las tecnologías utilizadas, los desafíos afrontados y los resultados obtenidos.`;
+
+      DescProyecto4.innerText =`Se trata de una descripción más detallada del proyecto.
+      Puede incluir información sobre los objetivos, las tecnologías utilizadas, los desafíos afrontados y los resultados obtenidos.`;
+
+      DescProyecto5.innerText =`Se trata de una descripción más detallada del proyecto.
+      Puede incluir información sobre los objetivos, las tecnologías utilizadas, los desafíos afrontados y los resultados obtenidos.`;
+
+  }
+});
---
+++ pages/aboutme/aboutme.js
@@ -0,0 +1,107 @@
+// Calcula la edad basada en la fecha de nacimiento
+function calcularEdad(fechaNacimiento) {
+  const hoy = new Date();
+  const nacimiento = new Date(fechaNacimiento);
+  let edad = hoy.getFullYear() - nacimiento.getFullYear();
+  const mes = hoy.getMonth() - nacimiento.getMonth();
+  if (mes < 0 || (mes === 0 && hoy.getDate() < nacimiento.getDate())) {
+    edad--;
+  }
+  return edad;
+}
+
+// Inserta la edad en el elemento con id 'edad'
+function actualizarEdad() {
+  const fechaNacimiento = "2000-11-02"; // Cambia esta fecha por tu fecha de nacimiento
+  const edad = calcularEdad(fechaNacimiento);
+  document.getElementById("edad").textContent = edad;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Al cargar la página, actualiza la edad
+  actualizarEdad();
+});
+
+document.getElementById("languageSwitch").addEventListener("change", function () {
+  const AboutMeInfo = document.getElementById("AboutMeInfo");
+  const BtnInicio = document.getElementById("BtnInicio");
+  const BtnSobreMi = document.getElementById("BtnSobreMi");
+  const BtnContacto = document.getElementById("BtnContacto");
+
+  if (this.checked) {
+    localStorage.setItem("language", "en");
+    BtnInicio.innerText = "Home";
+    BtnSobreMi.innerText = "About me";
+    BtnContacto.innerText = "Contact";
+    AboutMeInfo.innerHTML = `<h4>Hello 👋 I'm Lucas Vázquez, and I am <span id="edad"></span> years old.
+    <br>I am a Data Analyst and Associate's degree in university programming, graduated from <a href="https://www.frt.utn.edu.ar/">UTN FRT</a>.
+    <br><br>The technologies I work with include SQL, Python, Excel, Google Sheets, Looker Studio, Power BI, Power Query, and DAX.
+    I also plan to continue expanding my data knowledge with tools like machine learning and cloud computing platforms (e.g., AWS).
+
+    <br><br>You can check out some of my work and certifications on this website, in my resume, or through any of my social media platforms.
+    <br><br>If you want to contact me, you can do so via the contact form on the contact page or by sending me a direct message on any platform.
+    <br>Thank you for reading this far.
+    </h4>`;
+    
+    // Actualiza la edad después de cambiar el idioma
+    actualizarEdad();
+  } else {
+    localStorage.setItem("language", "es");
+    BtnInicio.innerText = "Inicio";
+    BtnSobreMi.innerText = "Sobre mí";
+    BtnContacto.innerText = "Contacto";
+    AboutMeInfo.innerHTML = `<h4>Hola 👋 Soy Lucas Vázquez y tengo <span id="edad"></span> años.
+    <br>Soy Analista de Datos y Técnico Universitario en Programación egresado de <a href="https://www.frt.utn.edu.ar/">UTN FRT</a>.
+    <br><br>Las tecnologías que manejo son SQL, Python, Excel, Google Sheets, Looker Studio, Power BI, Power Query, DAX.
+    También planeo continuar formándome en datos en herramientas como machine learning y plataformas de computación en la nube (Ej: AWS).
+
+    <br><br>Puedes revisar algunos de mis trabajos y certificaciones en esta página web, en mi CV o a través de cualquiera de mis redes.
+    <br><br>Si quieres contactarme puedes hacerlo desde el formulario en la pág de contacto o por MD por cualquier medio.
+    <br>Gracias por llegar hasta aquí.
+    </h4>`;
+
+    // Actualiza la edad después de cambiar el idioma
+    actualizarEdad();
+  }
+});
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  const language = localStorage.getItem("language");
+  if (language === "en") {
+    document.getElementById("languageSwitch").checked = true;
+    document.getElementById("BtnInicio").innerText = "Home";
+    document.getElementById("BtnSobreMi").innerText = "About me";
+    document.getElementById("BtnContacto").innerText = "Contact";
+    AboutMeInfo.innerHTML = `<h4>Hello 👋 I'm Lucas Vázquez, and I am <span id="edad"></span> years old.
+    <br>I am a Data Analyst and Associate's degree in university programming, graduated from <a href="https://www.frt.utn.edu.ar/">UTN FRT</a>.
+    <br><br>The technologies I work with include SQL, Python, Excel, Google Sheets, Looker Studio, Power BI, Power Query, and DAX.
+    I also plan to continue expanding my data knowledge with tools like machine learning and cloud computing platforms (e.g., AWS).
+
+    <br><br>You can check out some of my work and certifications on this website, in my resume, or through any of my social media platforms.
+    <br><br>If you want to contact me, you can do so via the contact form on the contact page or by sending me a direct message on any platform.
+    <br>Thank you for reading this far.
+    </h4>`;
+    
+    // Actualiza la edad después de cambiar el idioma
+    actualizarEdad();
+  } else {
+    document.getElementById("languageSwitch").checked = false;
+    document.getElementById("BtnInicio").innerText = "Inicio";
+    document.getElementById("BtnSobreMi").innerText = "Sobre mí";
+    document.getElementById("BtnContacto").innerText = "Contacto";
+    AboutMeInfo.innerHTML = `<h4>Hola 👋 Soy Lucas Vázquez y tengo <span id="edad"></span> años.
+    <br>Soy Analista de Datos y Técnico Universitario en Programación egresado de <a href="https://www.frt.utn.edu.ar/">UTN FRT</a>.
+    <br><br>Las tecnologías que manejo son SQL, Python, Excel, Google Sheets, Looker Studio, Power BI, Power Query, DAX.
+    También planeo continuar formándome en datos en herramientas como machine learning y plataformas de computación en la nube (Ej: AWS).
+
+    <br><br>Puedes revisar algunos de mis trabajos y certificaciones en esta página web, en mi CV o a través de cualquiera de mis redes.
+    <br><br>Si quieres contactarme puedes hacerlo desde el formulario en la pág de contacto o por MD por cualquier medio.
+    <br>Gracias por llegar hasta aquí.
+    </h4>`;
+
+    // Actualiza la edad después de cambiar el idioma
+    actualizarEdad();
+  }
+});
