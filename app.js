ed49d46fe921cb99f15755d4498bf869e16509f4
Lucas Vazquez <vazquez.lucas.alejo@gmail.com>
2024-11-29 21:54:55 -0300

    add foto y certificados, switch de idioma actualizado

--- app.js
+++ app.js
@@ -1,53 +1,63 @@
-document.getElementById("languageSwitch").addEventListener("change", function () {
-  const presentacion = document.getElementById("presentacion");
-  const Proyectos = document.getElementById("Proyectos");
-  const Certificados = document.getElementById("Certificados");
-  
-  const DescProyecto1 = document.getElementById("DescProyecto1");
-  const DescProyecto2 = document.getElementById("DescProyecto2");
-  const DescProyecto3 = document.getElementById("DescProyecto3");
-  const DescProyecto4 = document.getElementById("DescProyecto4");
-  const DescProyecto5 = document.getElementById("DescProyecto5");
-  
-  const BtnInicio = document.getElementById("BtnInicio");
-  const BtnSobreMi = document.getElementById("BtnSobreMi");
-  const BtnContacto = document.getElementById("BtnContacto");
+//texto presentacion
+const presentacion = document.getElementById("presentacion");
+//Botones del nav
+const BtnInicio = document.getElementById("BtnInicio");
+const BtnSobreMi = document.getElementById("BtnSobreMi");
+const BtnContacto = document.getElementById("BtnContacto");
+// Descripcion proyectos
+const DescProyecto1 = document.getElementById("DescProyecto1");
+const DescProyecto2 = document.getElementById("DescProyecto2");
+const DescProyecto3 = document.getElementById("DescProyecto3");
+const DescProyecto4 = document.getElementById("DescProyecto4");
+const DescProyecto5 = document.getElementById("DescProyecto5");
+//Titulos certificaciones
+const Proyectos = document.getElementById("Proyectos"); 
+const Certificados = document.getElementById("Certificados");
+const tecnologias = document.getElementById("tecnologias");
+const proximamente = document.getElementById("proximamente");
+//botones certificaciones
+const BtnCertificacion = document.querySelectorAll(".BtnCertificacion");
 
+document.getElementById("languageSwitch").addEventListener("change", function () {
   if (this.checked) {
     localStorage.setItem("language", "en");
     BtnInicio.innerText ="Home";
     BtnSobreMi.innerText ="About me";
     BtnContacto.innerText ="Contact";
+    presentacion.innerHTML = `
+    <span>
+    The page is currently under construction.<br><br>
+    I am a data analyst. I graduated as Associate's degree in university programming at <a href="https://www.frt.utn.edu.ar/">UTN FRT</a>.<br>
+    Some of the skills I have include: Python, PowerBI, Looker Studio, Excel, Spreadsheets, MySQL, and inferential statistics.
+    </span>`;
+    DescProyecto1.innerText =`This is a more detailed description of the project. It can include information about the goals, 
+    the technologies used, challenges faced, and the outcomes achieved.`;
+    
+    DescProyecto2.innerText =`This is a more detailed description of the project. It can include information about the goals, 
+    the technologies used, challenges faced, and the outcomes achieved.`;
+    
+    DescProyecto3.innerText =`This is a more detailed description of the project. It can include information about the goals, 
+    the technologies used, challenges faced, and the outcomes achieved.`;
+    
+    DescProyecto4.innerText =`This is a more detailed description of the project. It can include information about the goals, 
+    the technologies used, challenges faced, and the outcomes achieved.`;
+    
+    DescProyecto5.innerText =`This is a more detailed description of the project. It can include information about the goals, 
+    the technologies used, challenges faced, and the outcomes achieved.`;
     Proyectos.innerText ="Projects";
     Certificados.innerText = "Certifications";
-    presentacion.innerHTML = `
-      <span>
-        The page is currently under construction.<br><br>
-        I am a data analyst. I graduated as Associate's degree in university programming at <a href="https://www.frt.utn.edu.ar/">UTN FRT</a>.<br>
-        Some of the skills I have include: Python, PowerBI, Looker Studio, Excel, Spreadsheets, MySQL, and inferential statistics.
-      </span>`;
-      DescProyecto1.innerText =`This is a more detailed description of the project. It can include information about the goals, 
-      the technologies used, challenges faced, and the outcomes achieved.`;
+    tecnologias.innerText = "My Stack";
+    proximamente.innerText = "Soon..";
 
-      DescProyecto2.innerText =`This is a more detailed description of the project. It can include information about the goals, 
-      the technologies used, challenges faced, and the outcomes achieved.`;
+      BtnCertificacion.forEach((boton) => {
+        boton.textContent = "View Certification";
+      });
 
-      DescProyecto3.innerText =`This is a more detailed description of the project. It can include information about the goals, 
-      the technologies used, challenges faced, and the outcomes achieved.`;
-
-      DescProyecto4.innerText =`This is a more detailed description of the project. It can include information about the goals, 
-      the technologies used, challenges faced, and the outcomes achieved.`;
-
-      DescProyecto5.innerText =`This is a more detailed description of the project. It can include information about the goals, 
-      the technologies used, challenges faced, and the outcomes achieved.`;
-    
   } else {
     localStorage.setItem("language", "es");
     BtnInicio.innerText ="Inicio";
     BtnSobreMi.innerText ="Sobre mí";
     BtnContacto.innerText ="Contacto";
-    Proyectos.innerText ="Proyectos";
-    Certificados.innerText = "Certificados";
     presentacion.innerHTML = `
       <span>
         La página actualmente se encuentra en construcción.<br><br>
@@ -56,19 +66,27 @@ document.getElementById("languageSwitch").addEventListener("change", function ()
       </span>`;
       DescProyecto1.innerText =`Se trata de una descripción más detallada del proyecto.
       Puede incluir información sobre los objetivos, las tecnologías utilizadas, los desafíos afrontados y los resultados obtenidos.`;
-
+      
       DescProyecto2.innerText =`Se trata de una descripción más detallada del proyecto.
       Puede incluir información sobre los objetivos, las tecnologías utilizadas, los desafíos afrontados y los resultados obtenidos.`;
-
+      
       DescProyecto3.innerText =`Se trata de una descripción más detallada del proyecto.
       Puede incluir información sobre los objetivos, las tecnologías utilizadas, los desafíos afrontados y los resultados obtenidos.`;
-
+      
       DescProyecto4.innerText =`Se trata de una descripción más detallada del proyecto.
       Puede incluir información sobre los objetivos, las tecnologías utilizadas, los desafíos afrontados y los resultados obtenidos.`;
-
+      
       DescProyecto5.innerText =`Se trata de una descripción más detallada del proyecto.
       Puede incluir información sobre los objetivos, las tecnologías utilizadas, los desafíos afrontados y los resultados obtenidos.`;
+      
+      Proyectos.innerText ="Proyectos";
+      Certificados.innerText = "Certificados";
+      tecnologias.innerText = "Tecnologías que manejo";
+      proximamente.innerText = "Próximamente";
 
+      BtnCertificacion.forEach((boton) => {
+        boton.textContent = "Ver certificación";
+      });
   }
 });
 
@@ -78,38 +96,42 @@ window.addEventListener("DOMContentLoaded", () => {
   const language = localStorage.getItem("language");
   if (language === "en") {
     document.getElementById("languageSwitch").checked = true;
-    document.getElementById("BtnInicio").innerText = "Home";
-    document.getElementById("BtnSobreMi").innerText = "About me";
-    document.getElementById("BtnContacto").innerText = "Contact";
+    BtnInicio.innerText ="Home";
+    BtnSobreMi.innerText ="About me";
+    BtnContacto.innerText ="Contact";
+    presentacion.innerHTML = `
+    <span>
+    The page is currently under construction.<br><br>
+    I am a data analyst. I graduated as Associate's degree in university programming at <a href="https://www.frt.utn.edu.ar/">UTN FRT</a>.<br>
+    Some of the skills I have include: Python, PowerBI, Looker Studio, Excel, Spreadsheets, MySQL, and inferential statistics.
+    </span>`;
+    DescProyecto1.innerText =`This is a more detailed description of the project. It can include information about the goals, 
+    the technologies used, challenges faced, and the outcomes achieved.`;
+    
+    DescProyecto2.innerText =`This is a more detailed description of the project. It can include information about the goals, 
+    the technologies used, challenges faced, and the outcomes achieved.`;
+    
+    DescProyecto3.innerText =`This is a more detailed description of the project. It can include information about the goals, 
+    the technologies used, challenges faced, and the outcomes achieved.`;
+    
+    DescProyecto4.innerText =`This is a more detailed description of the project. It can include information about the goals, 
+    the technologies used, challenges faced, and the outcomes achieved.`;
+    
+    DescProyecto5.innerText =`This is a more detailed description of the project. It can include information about the goals, 
+    the technologies used, challenges faced, and the outcomes achieved.`;
     Proyectos.innerText ="Projects";
     Certificados.innerText = "Certifications";
-    presentacion.innerHTML = `
-      <span>
-        The page is currently under construction.<br><br>
-        I am a data analyst. I graduated as Associate's degree in university programming at <a href="https://www.frt.utn.edu.ar/">UTN FRT</a>.<br>
-        Some of the skills I have include: Python, PowerBI, Looker Studio, Excel, Spreadsheets, MySQL, and inferential statistics.
-      </span>`;
-      DescProyecto1.innerText =`This is a more detailed description of the project. It can include information about the goals, 
-      the technologies used, challenges faced, and the outcomes achieved.`;
+    tecnologias.innerText = "My Stack";
+    proximamente.innerText = "Soon..";
 
-      DescProyecto2.innerText =`This is a more detailed description of the project. It can include information about the goals, 
-      the technologies used, challenges faced, and the outcomes achieved.`;
+      BtnCertificacion.forEach((boton) => {
+        boton.textContent = "View Certification";
+      });
 
-      DescProyecto3.innerText =`This is a more detailed description of the project. It can include information about the goals, 
-      the technologies used, challenges faced, and the outcomes achieved.`;
-
-      DescProyecto4.innerText =`This is a more detailed description of the project. It can include information about the goals, 
-      the technologies used, challenges faced, and the outcomes achieved.`;
-
-      DescProyecto5.innerText =`This is a more detailed description of the project. It can include information about the goals, 
-      the technologies used, challenges faced, and the outcomes achieved.`;
   } else {
-    document.getElementById("languageSwitch").checked = false;
-    document.getElementById("BtnInicio").innerText = "Inicio";
-    document.getElementById("BtnSobreMi").innerText = "Sobre mí";
-    document.getElementById("BtnContacto").innerText = "Contacto";
-    Proyectos.innerText ="Proyectos";
-    Certificados.innerText = "Certificados";
+    BtnInicio.innerText ="Inicio";
+    BtnSobreMi.innerText ="Sobre mí";
+    BtnContacto.innerText ="Contacto";
     presentacion.innerHTML = `
       <span>
         La página actualmente se encuentra en construcción.<br><br>
@@ -118,18 +140,26 @@ window.addEventListener("DOMContentLoaded", () => {
       </span>`;
       DescProyecto1.innerText =`Se trata de una descripción más detallada del proyecto.
       Puede incluir información sobre los objetivos, las tecnologías utilizadas, los desafíos afrontados y los resultados obtenidos.`;
-
+      
       DescProyecto2.innerText =`Se trata de una descripción más detallada del proyecto.
       Puede incluir información sobre los objetivos, las tecnologías utilizadas, los desafíos afrontados y los resultados obtenidos.`;
-
+      
       DescProyecto3.innerText =`Se trata de una descripción más detallada del proyecto.
       Puede incluir información sobre los objetivos, las tecnologías utilizadas, los desafíos afrontados y los resultados obtenidos.`;
-
+      
       DescProyecto4.innerText =`Se trata de una descripción más detallada del proyecto.
       Puede incluir información sobre los objetivos, las tecnologías utilizadas, los desafíos afrontados y los resultados obtenidos.`;
-
+      
       DescProyecto5.innerText =`Se trata de una descripción más detallada del proyecto.
       Puede incluir información sobre los objetivos, las tecnologías utilizadas, los desafíos afrontados y los resultados obtenidos.`;
+      
+      Proyectos.innerText ="Proyectos";
+      Certificados.innerText = "Certificados";
+      tecnologias.innerText = "Tecnologías que manejo";
+      proximamente.innerText = "Próximamente";
 
+      BtnCertificacion.forEach((boton) => {
+        boton.textContent = "Ver certificación";
+      });
   }
 });
--- pages/aboutme/aboutme.js
+++ pages/aboutme/aboutme.js
@@ -54,13 +54,10 @@ document.getElementById("languageSwitch").addEventListener("change", function ()
     <br>Soy Analista de Datos y Técnico Universitario en Programación egresado de <a href="https://www.frt.utn.edu.ar/">UTN FRT</a>.
     <br><br>Las tecnologías que manejo son SQL, Python, Excel, Google Sheets, Looker Studio, Power BI, Power Query, DAX.
     También planeo continuar formándome en datos en herramientas como machine learning y plataformas de computación en la nube (Ej: AWS).
-
     <br><br>Puedes revisar algunos de mis trabajos y certificaciones en esta página web, en mi CV o a través de cualquiera de mis redes.
     <br><br>Si quieres contactarme puedes hacerlo desde el formulario en la pág de contacto o por MD por cualquier medio.
     <br>Gracias por llegar hasta aquí.
     </h4>`;
-
-    // Actualiza la edad después de cambiar el idioma
     actualizarEdad();
   }
 });
@@ -78,24 +75,21 @@ window.addEventListener("DOMContentLoaded", () => {
     <br>I am a Data Analyst and Associate's degree in university programming, graduated from <a href="https://www.frt.utn.edu.ar/">UTN FRT</a>.
     <br><br>The technologies I work with include SQL, Python, Excel, Google Sheets, Looker Studio, Power BI, Power Query, and DAX.
     I also plan to continue expanding my data knowledge with tools like machine learning and cloud computing platforms (e.g., AWS).
-
     <br><br>You can check out some of my work and certifications on this website, in my resume, or through any of my social media platforms.
     <br><br>If you want to contact me, you can do so via the contact form on the contact page or by sending me a direct message on any platform.
     <br>Thank you for reading this far.
     </h4>`;
-    
-    // Actualiza la edad después de cambiar el idioma
     actualizarEdad();
   } else {
     document.getElementById("languageSwitch").checked = false;
     document.getElementById("BtnInicio").innerText = "Inicio";
     document.getElementById("BtnSobreMi").innerText = "Sobre mí";
     document.getElementById("BtnContacto").innerText = "Contacto";
-    AboutMeInfo.innerHTML = `<h4>Hola 👋 Soy Lucas Vázquez y tengo <span id="edad"></span> años.
+    AboutMeInfo.innerHTML = `
+    <h4>Hola 👋 Soy Lucas Vázquez y tengo <span id="edad"></span> años.
     <br>Soy Analista de Datos y Técnico Universitario en Programación egresado de <a href="https://www.frt.utn.edu.ar/">UTN FRT</a>.
     <br><br>Las tecnologías que manejo son SQL, Python, Excel, Google Sheets, Looker Studio, Power BI, Power Query, DAX.
     También planeo continuar formándome en datos en herramientas como machine learning y plataformas de computación en la nube (Ej: AWS).
-
     <br><br>Puedes revisar algunos de mis trabajos y certificaciones en esta página web, en mi CV o a través de cualquiera de mis redes.
     <br><br>Si quieres contactarme puedes hacerlo desde el formulario en la pág de contacto o por MD por cualquier medio.
     <br>Gracias por llegar hasta aquí.
